516b953363a8883ddc58fec898ece53c0617c248
anilac13 <anil4u2496@gmail.com>
2023-05-23 12:35:39 +0530

    [Anil] Add:Customize the message given

---
+++ DataStructuresAndAlgorithms/CustomizeString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class CustomizeString
+    {
+        public void Customize()
+        {
+            string msg = "Hello <<name>>, We have your full name as <<full name>> in our system. your contact number is 91-xxxxxxxxxx. Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016.";
+            msg = msg.Replace("<<name>>", "Anil");
+            msg = msg.Replace("<<full name>>", "Anil Kumar");
+            msg = msg.Replace("91-xxxxxxxxxx", "91-9856745643");
+            msg = msg.Replace("01/01/2016", DateTime.Today.ToShortDateString());
+            Console.WriteLine(msg);
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -11,7 +11,8 @@ namespace DataStructuresAndAlgorithms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose option to perform \n1.Binary Search \n2.Permutations of a String \n3.Insertion Sort \n4.Bubble Sort \n5.Check Anagram \n6.Prime range \n7.Exit");
+                Console.WriteLine("\nChoose option to perform \n1.Binary Search \n2.Permutations of a String \n3.Insertion Sort \n4.Bubble Sort \n5.Check Anagram \n6.Prime range " +
+                    "\n7.Customize String \n8.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -42,6 +43,10 @@ namespace DataStructuresAndAlgorithms
                         primeRange.PrimeNumbers();
                         break;
                     case 7:
+                        CustomizeString customizeString = new CustomizeString();
+                        customizeString.Customize();
+                        break;
+                    case 8:
                         flag = false;
                         break;
                 }
