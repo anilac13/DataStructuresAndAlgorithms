a7946381f991a1b0da626eb0b9f43fa34fe3a005
anilac13 <anil4u2496@gmail.com>
2023-05-23 11:54:01 +0530

    [Anil] Add:Sort string using Insertion sort

---
+++ DataStructuresAndAlgorithms/InsertionSortProblem/InsertionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.InsertionSortProblem
+{
+    public class InsertionSort
+    {
+        public void Sort(string filePath)
+        {
+            string readData = File.ReadAllText(filePath);
+            string[] words = readData.Split(" ");
+            foreach (var data in words)
+            {
+                Console.Write(data + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (words[i].CompareTo(words[j]) > 0)
+                    {
+                        string temp = words[i];
+                        words[i] = words[j];
+                        words[j] = temp;
+                    }
+                }
+            }
+            foreach (var data in words)
+            {
+                Console.Write(data + " ");
+            }
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DataStructuresAndAlgorithms.BinarySearchProblem;
+using DataStructuresAndAlgorithms.InsertionSortProblem;
 using System;
 namespace DataStructuresAndAlgorithms
 {
@@ -9,7 +10,7 @@ namespace DataStructuresAndAlgorithms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose option to perform \n1.Binary Search \n2.Permutations of a String \n3.Exit");
+                Console.WriteLine("\nChoose option to perform \n1.Binary Search \n2.Permutations of a String \n3.Insertion Sort \n4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -24,6 +25,10 @@ namespace DataStructuresAndAlgorithms
                         permutations.Permutation(a, 0, b - 1);
                         break;
                     case 3:
+                        InsertionSort sort = new InsertionSort();
+                        sort.Sort(@"D:\BridgeLabs\DataStructuresAndAlgorithms\DataStructuresAndAlgorithms\InsertionSortProblem\File.txt");
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
